f1bd0daf91e36c0d28cea321cbd220d07127c59f
buterinsisters <pourquoipas09@protonmail.com>
2021-02-06 15:19:23 +0100

    commit all files

---
+++ bot.js
@@ -0,0 +1,49 @@
+const Twit = require('twit');
+const config = require('./config.js');
+const tweetBot = new Twit(config);
+
+const params = {
+    q: '$based AND $Moonbase filter:media',
+    count: 5,
+    lang: 'en'
+}
+
+const favParams = {
+    q: '$based OR $basedghouls filter:media',
+    count: 5,
+    lang: 'en'
+}
+
+tweetBot.get('search/tweets', params,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Retweet successful`)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', favParams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Retweet successful`)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
